4504df09e9e05ba6078271d1d3324107a920a6cc
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-25 14:24:37 +0800

    test widgets

---
+++ src/app/production/page.tsx
@@ -0,0 +1,13 @@
+export default function Production() {
+  return (
+    <>
+      hello world
+      <iframe
+        src="https://better.giving/donate-widget/1?isDescriptionTextShown=true&splitDisabled=true&liquidSplitPct=38&methods=stripe%2Cdaf&isTitleShown=true&accentPrimary=%232D89C8&accentSecondary=%23E6F1F9"
+        width="700"
+        height="900"
+        style={{ border: "0px" }}
+      />
+    </>
+  );
+}
