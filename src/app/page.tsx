90d1eac5ee11a3601b3094a1b0262d5f520dd3ca
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-25 14:27:13 +0800

    remove text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 export default function Home() {
   return (
-    <>
-      hello world
-      <iframe
-        src="https://staging.better.giving/donate-widget/32?isDescriptionTextShown=true&splitDisabled=true&liquidSplitPct=38&methods=stripe%2Cdaf&isTitleShown=true&accentPrimary=%232D89C8&accentSecondary=%23E6F1F9"
-        width="700"
-        height="900"
-        style={{ border: "0px" }}
-      />
-    </>
+    <iframe
+      src="https://staging.better.giving/donate-widget/32?isDescriptionTextShown=true&splitDisabled=true&liquidSplitPct=38&methods=stripe%2Cdaf&isTitleShown=true&accentPrimary=%232D89C8&accentSecondary=%23E6F1F9"
+      width="700"
+      height="900"
+      style={{ border: "0px" }}
+    />
   );
 }
